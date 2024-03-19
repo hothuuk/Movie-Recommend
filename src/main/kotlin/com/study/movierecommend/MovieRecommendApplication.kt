8c0a1b003b196e30b5186c9476e1a9fb9bf644df
hothuuk <hothuuk@gmail.com>
2024-03-19 12:21:25 +0900

    add :: 어노테이션 추가

--- src/main/kotlin/com/study/movierecommend/MovieRecommendApplication.kt
+++ src/main/kotlin/com/study/movierecommend/MovieRecommendApplication.kt
@@ -1,9 +1,13 @@
 package com.study.movierecommend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
+@ConfigurationPropertiesScan
 class MovieRecommendApplication
 
 fun main(args: Array<String>) {
