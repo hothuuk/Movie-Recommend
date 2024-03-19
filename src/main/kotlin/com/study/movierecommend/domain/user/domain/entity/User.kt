5dac148b5ceb46adbf26f4b057e301e9c55080b7
hothuuk <hothuuk@gmail.com>
2024-03-19 16:00:59 +0900

    create :: User

---
+++ src/main/kotlin/com/study/movierecommend/domain/user/domain/entity/User.kt
@@ -0,0 +1,28 @@
+package com.study.movierecommend.domain.user.domain.entity
+
+import com.study.movierecommend.domain.user.enums.Role
+import javax.persistence.*
+
+@Entity
+@Table(name = "user")
+class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    val id: Long,
+
+    @Column(name = "user_email", nullable = false, unique = true)
+    val email: String,
+
+    @Column(name = "user_password", nullable = false)
+    val password: String,
+
+    @Column(name = "user_name", nullable = false)
+    val name: String,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "Role")
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "Role", joinColumns = [JoinColumn(name = "user_id")])
+    var roles: MutableList<Role>
+)
