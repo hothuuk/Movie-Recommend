9c637d37922c9b205f56ce657d0711fe9fd6cda8
hothuuk <hothuuk@gmail.com>
2024-03-19 20:14:53 +0900

    create :: AuthConverterImpl

---
+++ src/main/kotlin/com/study/movierecommend/domain/auth/util/impl/AuthConverterImpl.kt
@@ -0,0 +1,25 @@
+package com.study.movierecommend.domain.auth.util.impl
+
+import com.study.movierecommend.domain.auth.presentation.data.dto.SignInDto
+import com.study.movierecommend.domain.auth.presentation.data.dto.SignUpDto
+import com.study.movierecommend.domain.auth.presentation.data.req.SignInReqDto
+import com.study.movierecommend.domain.auth.presentation.data.req.SignUpReqDto
+import com.study.movierecommend.domain.auth.util.AuthConverter
+import org.springframework.stereotype.Component
+
+@Component
+class AuthConverterImpl : AuthConverter {
+
+    override fun toDto(signUpReqDto: SignUpReqDto): SignUpDto =
+        SignUpDto(
+            email = signUpReqDto.email,
+            password = signUpReqDto.password,
+            name = signUpReqDto.name
+        )
+
+    override fun toDto(signInReqDto: SignInReqDto): SignInDto =
+        SignInDto(
+            email = signInReqDto.email,
+            password = signInReqDto.password
+        )
+}
