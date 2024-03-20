877bd0b15e8f901769aa72c4390993d096ba3c91
hothuuk <hothuuk@gmail.com>
2024-03-20 17:43:48 +0900

    create :: RefreshResDto

---
+++ src/main/kotlin/com/study/movierecommend/domain/auth/presentation/data/res/RefreshResDto.kt
@@ -0,0 +1,17 @@
+package com.study.movierecommend.domain.auth.presentation.data.res
+
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.study.movierecommend.domain.user.enums.Role
+import java.time.ZonedDateTime
+
+data class RefreshResDto(
+    val accessToken: String,
+    val refreshToken: String,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    val accessExp: ZonedDateTime,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    val refreshExp: ZonedDateTime,
+    val roles: MutableList<Role>,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    val expiresAt: ZonedDateTime
+)
