8187a9aef2e75fb02f20a4312d9acbd1dfd60176
hothuuk <hothuuk@gmail.com>
2024-03-22 09:22:50 +0900

    add :: findMovieByUser 추가

--- src/main/kotlin/com/study/movierecommend/domain/movie/domain/repository/UserMovieHistoryRepository.kt
+++ src/main/kotlin/com/study/movierecommend/domain/movie/domain/repository/UserMovieHistoryRepository.kt
@@ -4,8 +4,13 @@ import com.study.movierecommend.domain.movie.domain.entity.Movie
 import com.study.movierecommend.domain.movie.domain.entity.UserMovieHistory
 import com.study.movierecommend.domain.user.domain.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface UserMovieHistoryRepository : JpaRepository<UserMovieHistory, Long> {
 
     fun existsByUserAndMovie(user: User, movie: Movie): Boolean
+
+    @Query("select umh.movie from UserMovieHistory umh where umh.user = :user")
+    fun findMovieByUser(@Param("user") user: User): List<Movie>
 }
